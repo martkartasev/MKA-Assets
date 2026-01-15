6ae45ef3003b4f5510f100e086bf1543bd9f6bcd
Mart Kartasev <mart.kartasev@gmail.com>
2026-01-15 10:22:49 +0100

    Simple datastructure

---
+++ Runtime/Scripts/DataStructures.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Scripts
+{
+    [Serializable]
+    public struct RangeFloat
+    {
+        public float min;
+        public float max;
+        
+        public RangeFloat(float min, float max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
